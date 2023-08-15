c72b477db15f26f334b15b3e88dcda82204453a1
Tomislav Busic <tomislav.busic89@gmail.com>
2023-08-15 14:22:42 +0200

    polymorphism

--- OOPCSharp/Program.cs
+++ OOPCSharp/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using OOPCSharp;
+
 Console.WriteLine("Hello, World!");
+
+Dog myDog = new();
+myDog.DogBarking();
+
+IDogWalker otherDog = new Dog();
