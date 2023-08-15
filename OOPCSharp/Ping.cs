1805c163a2343787a64840105a9ff5a443280a3f
Tomislav Busic <tomislav.busic89@gmail.com>
2023-08-15 18:26:17 +0200

    polymorphism

---
+++ OOPCSharp/Ping.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPCSharp
+{
+    public class Ping
+    {
+        public int HostAddress { get; set; }
+        public int DestAddress { get; set; }
+        public string Data { get; set; }
+
+        // If ping boots, return true
+        public bool Init()
+        {
+            return true;
+        }
+
+        public bool SendPing()
+        {
+            return true;
+        }
+    }
+}
--- OOPCSharp/Program.cs
+++ OOPCSharp/Program.cs
@@ -14,3 +14,6 @@ otherDog.WalkToThePark();
 Console.WriteLine(newLine);
 
 //////////////////////////////////
+
+Console.WriteLine("Abstract Classes");
+
