466538105e35fb49bb3993f6b8ee07c1c831ff24
Tomislav Busic <tomislav.busic89@gmail.com>
2023-08-16 17:37:27 +0200

    Pass by REference

--- OOPCSharp/PassByReference.cs
+++ OOPCSharp/PassByReference.cs
@@ -9,10 +9,16 @@ namespace OOPCSharp
     public class PassByReference
     {
         public PassByReference() {
+            // Pass By Value VS. Pass By Reference
+
+            // Reference
+
             Person person1 = new();
             Person person2 = new();
             person1.Age = 5;
             person2.Age = 10;
+
+            Console.WriteLine(person1.Age + " " + person2.Age);
         }
      }
 }
