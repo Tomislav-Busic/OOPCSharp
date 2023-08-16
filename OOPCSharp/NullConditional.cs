8937c4160e3c1204e1204c89cc829f1191a9fbce
Tomislav Busic <tomislav.busic89@gmail.com>
2023-08-16 19:06:50 +0200

    Polymorphism

---
+++ OOPCSharp/NullConditional.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPCSharp
+{
+    public class NullConditional
+    {
+        public NullConditional()
+        {
+            // NULL Conditional and Coalescing Operators
+
+            string myString = null;
+
+            if (myString == null)
+            {
+                Console.WriteLine("This string is null");
+            } else
+            {
+                Console.WriteLine(myString);
+            }
+        }
+    }
+}
