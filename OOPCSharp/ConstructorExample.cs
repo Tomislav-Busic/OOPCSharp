4f471ae3a373166cf05fde8361d6ef3bdcef6366
Tomislav Busic <tomislav.busic89@gmail.com>
2023-08-16 19:25:16 +0200

    Null Conditional

---
+++ OOPCSharp/ConstructorExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPCSharp
+{
+    public class ConstructorExample
+    {
+        public string Name { get; set; }
+        public ConstructorExample(string name)
+        {
+            Name = name;
+        }
+    }
+}
